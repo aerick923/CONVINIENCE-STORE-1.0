49894e0c990ca3f8f701d05fb68c444c7720ce85
Aerick Lee P. Alba <albaaericklee@gmail.com>
2026-02-08 17:04:45 +0800

    Add files via upload

---
+++ CONVINIENCESTORE_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.convinience.store_1;
+
+/**
+ *
+ * @author USER
+ */
+
+// FEBRUARY 8, 2026: STARTING POINT
+public class CONVINIENCESTORE_1 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("===== WELCOME TO THE CONVINIENCE STORE! =====");
+        System.out.println("[1] CREATE ACCOUNT");
+        System.out.println("[2] MENU");
+        System.out.println("[3] ATM");
+        System.out.println("[4] EXIT PROGRAM");
+    }
+}
